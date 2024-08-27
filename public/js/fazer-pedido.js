ce3272967afa70d303c26171c102c427b7478138
Emmanuel-Moraes <emmanuelbmoraes@gmail.com>
2024-08-27 14:48:43 -0300

    feat: add remove item function to cart

--- public/js/fazer-pedido.js
+++ public/js/fazer-pedido.js
@@ -1,23 +1,36 @@
-window.addEventListener('load', fazerPedido)
+window.addEventListener("load", fazerPedido);
 
-const cartItens = document.getElementById('lista-itens')
-const totalPedido = document.getElementById('total-pedido')
-let total=0
+const cartItens = document.getElementById("lista-itens");
+const totalPedido = document.getElementById("total-pedido");
+let total = 0;
 
-function fazerPedido(){
-    const cart = localStorage.getItem('cart')
-    if(cart){
-        const produtos = JSON.parse(cart)
-        for(p of produtos){
-            total += p.price
-            cartItens.innerHTML +=  `
-            <li>
-              <img src=${"../img/"+p.image_url} alt=${p.name}>
-              <p>${p.name}</p>
-              <p>R$${(p.price).toFixed(2)}</p>
-            </li>
-            `
-        }
-        totalPedido.innerHTML = `Total: R$${(total).toFixed(2)}`
-    }
+function fazerPedido() {
+  const cart = localStorage.getItem("cart");
+  const produtos = JSON.parse(cart);
+  if (cart) {
+    cartItens.innerHTML = "";
+    let total = 0;
+
+    produtos.forEach((p, index) => {
+      total += p.price;
+      cartItens.innerHTML += `
+          <li data-index="${index}">
+            <img src=${"../img/" + p.image_url} alt=${p.name}>
+            <p>${p.name}</p>
+            <p>R$${p.price.toFixed(2)}</p>
+            <span class="remover-item" data-index="${index}"> X </span>
+          </li>
+          `;
+    });
+    totalPedido.innerHTML = `Total: R$${total.toFixed(2)}`;
+  }
+  const items = document.querySelectorAll(".remover-item");
+  items.forEach((item) => {
+    item.addEventListener("click", function () {
+      const index = this.getAttribute("data-index");
+      produtos.splice(index, 1);
+      localStorage.setItem("cart", JSON.stringify(produtos));
+      fazerPedido();
+    });
+  });
 }
