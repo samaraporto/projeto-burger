90ecb696bb4a4d75d76a4c83e3d59ee56ee6306b
Emmanuel-Moraes <emmanuelbmoraes@gmail.com>
2024-09-03 08:14:15 -0300

    fix: add cart refresh total on fazer-pedidos.js

--- public/js/fazer-pedido.js
+++ public/js/fazer-pedido.js
@@ -5,8 +5,9 @@ const totalPedido = document.getElementById("total-pedido");
 let total = 0;
 
 function fazerPedido() {
-  if(!localStorage.getItem('Token')){
-    localStorage.removeItem('cart')
+  total = 0;
+  if (!localStorage.getItem("Token")) {
+    localStorage.removeItem("cart");
   }
   const cart = localStorage.getItem("cart");
   const produtos = JSON.parse(cart) || [];
@@ -16,10 +17,9 @@ function fazerPedido() {
 
     produtos.forEach((p, index) => {
       total += Number(p.price);
-      console.log("preco",p.price);
-      console.log("total",total);
-      
-      
+      console.log("preco", p.price);
+      console.log("total", total);
+
       cartItens.innerHTML += `
           <li data-index="${index}">
             <img src=${"../img/" + p.image_url} alt=${p.name}>
@@ -29,11 +29,11 @@ function fazerPedido() {
           </li>
           `;
     });
-    
-    totalPedido.innerHTML = `Total: R$${total.toFixed(2)}`;
 
-  }else{
-    cartItens.innerHTML = "<li>Seu carrinho está vazio</li>";
+    totalPedido.innerHTML = `Total: R$${total.toFixed(2)}`;
+  } else {
+    cartItens.innerHTML =
+      "<li class='carrinho-vazio'>Seu carrinho está vazio</li>";
     totalPedido.innerHTML = "Total: R$0.00";
   }
 
@@ -47,8 +47,8 @@ function fazerPedido() {
     });
   });
 
-  document.querySelector('form').addEventListener('submit', ()=>{
-    localStorage.removeItem('cart')
-    alert('Pedido realizado com sucesso!')
-  })
+  document.querySelector("form").addEventListener("submit", () => {
+    localStorage.removeItem("cart");
+    alert("Pedido realizado com sucesso!");
+  });
 }
